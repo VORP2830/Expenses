8e6250bb87cec6d58c616ed669c2e9a05e0d1ef1
Victor Osório Ramos Porto <victor.porto@mpse.mp.br>
2024-04-10 10:33:45 -0300

    Mapeando objetos

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:expenses/models/transaction.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -13,6 +15,20 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomeApp extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+      id: 'T1',
+      title: 'Tenis de corrida',
+      value: 999.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 'T2',
+      title: 'Conta de energia',
+      value: 211.36,
+      date: DateTime.now(),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,21 +39,25 @@ class MyHomeApp extends StatelessWidget {
         backgroundColor: Colors.blue,
       ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
             width: double.infinity,
             child: Card(
+              color: Colors.blue,
               child: Text(
                 'Grafico',
               ),
               elevation: 5,
             ),
           ),
-          Card(
-            child: Text(
-              'Lista de Transações',
-            ),
+          Column(
+            children: _transactions.map((tr) {
+              return Card(
+                child: Text(tr.title),
+              );
+            }).toList(),
           ),
         ],
       ),
